fd6b7d2db02a7bd5c8988fbda18ca871492817c7
szupurgy <karcsirichi@gmail.com>
2024-01-12 19:18:57 +0100

    alakulgat

---
+++ webx/src/sources/selectorbar.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Selector = () => {
+  return (
+    
+    <div className=' w-screen h-screen fixed overflow-visible text-right justify-end flex'>
+        <div className='bg-gray-300 justify-center text-center w-64'>
+        <a href="#" className=''>Home</a>
+        <hr />
+        <a href="#Termekek" className=''>Termékek</a>
+        </div>
+    </div>
+  )
+}
+
+export default Selector
--- webx/src/sources/topbar.jsx
+++ webx/src/sources/topbar.jsx
@@ -1,14 +1,30 @@
-import React from 'react'
+import React, {useState} from 'react'
+import Selector from './selectorbar'
 
 const TopBar = () => {
+  const [rigtBar, setVisible] = useState(false);
   return (
+    <>
     <div className='bg-gray-500 overflow-hidden h-24 p-3 flex shadow-lg '>
         <img src="../public/logo.png" className='inline-flex w-24' title='WebX logo' alt="logo" />
-        <div className=''>
-        <a href="#">Home</a>
-        <a href="#Termekek">Termékek</a>
+        <div className='flex w-full justify-center text-center p-5 gap-6'>
+        <a href="#" className='lg:flex sm:block md:block hidden'>Home</a>
+        <a href="#Termekek" className='lg:block md:block sm:block hidden'>Termékek</a>
+        </div>
+        <div className=' justify-center text-center p-5'>
+        <button onClick={()=>setVisible(!rigtBar)}>
+          <img src="/public/menuicon.svg" className='w-8 flex sm:hidden lg:hidden md:hidden' />
+        </button>
         </div>
     </div>
+    <div id='new'>
+      {
+        rigtBar ? (
+          <Selector/>
+        ) : null
+      }
+    </div>
+    </>
   )
 }
 
