a8a37ed2111c97a6e3d37037c359eb68f55052f5
szupurgy <karcsirichi@gmail.com>
2024-01-24 17:47:28 +0100

    modositasok

--- webx/src/pages/login.jsx
+++ webx/src/pages/login.jsx
@@ -5,26 +5,28 @@ const Belepes = () => {
   return (
     <>
     <div className='w-screen h-screen justify-center items-center flex flex-col'>
-        <div className='bg-gray-50 m-6 p-10 shadow-md w-3/6 rounded-md border-stone-950 border-2'>
+        <div className='bg-gray-50 m-6 p-10 w-screen h-screen sm:h-auto sm:w-3/6 rounded-md border-stone-950 border-2 shadow-[rgba(0,0,0,0.25)_0px_54px_55px,rgba(0,0,0,0.12)_0px_-12px_30px,rgba(0,0,0,0.12)_0px_4px_6px,rgba(0,0,0,0.17)_0px_12px_13px,rgba(0,0,0,0.09)_0px_-3px_5px]'>
         <div>
             <a href="home">
             <img src="../public/logo.png" className='w-full h-full'/>
             </a>
         </div>
         <hr className=' shadow-lg ' />
-        <div className='flex '>
+        <div>
             <form className='inline-flex w-full' action="login">
                 <div className='w-1/2'>
-                <h1 className='p-2 text-xl'>Felhasználónév:</h1>
+                    <h1 className='p-2 text-xl'>Felhasználónév:</h1>
                     <input id='Name' type='text' placeholder='Név' className='border rounded-md p-2.5 shadow-md' />
                     <h1 className='p-2 text-xl'>Jelszó:</h1>
                     <input id='Password' type="password" placeholder='Jelszó' className='border rounded-md p-2.5 shadow-md' />
                     <br />
                     <a href="signup" className='text-blue-600'>Regisztráció</a>
+                    
+                        <div className=' flex justify-center items-center'>
+                            <button id='Belepes' type="submit" className='text-2xl m-6 p-3 border rounded-md bg-green-700'>Belépés!</button>
+                        </div>
+                   
                 </div>
-                    <div className='w-1/2 flex justify-center items-center '>
-                        <button id='Belepes' type="submit" className='text-2xl m-6 p-3 border rounded-md bg-green-700'>Belépés!</button>
-                    </div>
             </form>
         </div>
         </div>
--- webx/src/pages/singup.jsx
+++ webx/src/pages/singup.jsx
@@ -4,30 +4,30 @@ const SignUp = () => {
   return (
     <>
     <div className='w-screen h-screen justify-center items-center flex flex-col'>
-        <div className='bg-gray-50 m-6 p-10 w-3/6 rounded-md border-stone-950 border-2 shadow-[rgba(0,0,0,0.25)_0px_54px_55px,rgba(0,0,0,0.12)_0px_-12px_30px,rgba(0,0,0,0.12)_0px_4px_6px,rgba(0,0,0,0.17)_0px_12px_13px,rgba(0,0,0,0.09)_0px_-3px_5px]'>
+        <div className='bg-gray-50 p-10 w-screen h-screen sm:h-auto sm:w-1/2 rounded-md border-stone-950 border-2 shadow-[rgba(0,0,0,0.25)_0px_54px_55px,rgba(0,0,0,0.12)_0px_-12px_30px,rgba(0,0,0,0.12)_0px_4px_6px,rgba(0,0,0,0.17)_0px_12px_13px,rgba(0,0,0,0.09)_0px_-3px_5px]'>
         <div>
             <a href="home">
             <img src="../public/logo.png" className='w-full h-full'/>
             </a>
         </div>
         <hr className=' shadow-lg ' />
-        <div className='flex '>
+        <div className=''>
             <form className='inline-flex w-full' action="login">
-                <div className='w-1/2 '>
+                <div className='w-1/2'>
                     <h1 className='p-2 text-xl'>Teljes név:</h1>
-                    <input id='Name' type='text' placeholder='Név' className='border rounded-md p-2.5 shadow-md' />
+                    <input id='Name' type='text' placeholder='Név' className='border ring-2 rounded-md p-2.5 shadow-md' />
                     <h1 className='p-2 text-xl'>E-mail:</h1>
-                    <input id='Email' type='email' placeholder='E-mail' className='border rounded-md p-2.5 shadow-md' />
+                    <input id='Email' type='email' placeholder='E-mail' className='border ring-2 rounded-md p-2.5 shadow-md' />
                     <h1 className='p-2 text-xl'>Jelszó:</h1>
-                    <input id='Password' type="password" placeholder='Jelszó' className='border rounded-md p-2.5 shadow-md' />
+                    <input id='Password' type="password" placeholder='Jelszó' className='border ring-2 rounded-md p-2.5 shadow-md' />
                     <h1 className='p-2 text-xl'>Jelszó ismét:</h1>
-                    <input id='Password_again' type="password" placeholder='Jelszó ismét' className='border rounded-md p-2.5 shadow-md' />
+                    <input id='Password_again' type="password" placeholder='Jelszó ismét' className='border ring-2 rounded-md p-2.5 shadow-md' />
                     <br />
                     <a href="login" className='text-blue-600'>Belépés</a>
-                </div>
-                    <div className='w-1/2 flex justify-center items-center '>
+                    <div className=''>
                         <button id='Regisztracio' type="submit" className='text-2xl m-6 p-3 border rounded-md bg-green-700'>Regisztráció</button>
                     </div>
+                </div>
             </form>
         </div>
         </div>
