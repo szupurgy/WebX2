b6bb42182e0431e2f35d36c5a8d1f0ca53e0c24b
szupurgy <karcsirichi@gmail.com>
2024-02-14 19:48:45 +0100

    mukodoverzio

--- webx/src/components/signup/success.jsx
+++ webx/src/components/signup/success.jsx
@@ -1,8 +1,6 @@
 import React from 'react'
 
 const UtolsoLap = async(values) => {
-    console.log(values.values)
-    
   location.href="home";
   return (
     <div className='w-screen h-screen justify-center flex items-center text-4xl'>Sikeres Regisztráció</div>
--- webx/src/pages/Home.jsx
+++ webx/src/pages/Home.jsx
@@ -1,7 +1,5 @@
 import React from 'react'
 import TopBar from '../components/topbar/topbar'
-import { PrismaClient } from '@prisma/client'
-const prisma = new PrismaClient()
 
 
 const HomePage = () => {
