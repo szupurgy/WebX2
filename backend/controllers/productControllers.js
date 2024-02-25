ad7cc3aa345537a65d3c2921145a3cbed007c393
szupurgy <karcsirichi@gmail.com>
2024-02-25 19:44:26 +0100

    kosarbackend

--- backend/controllers/productControllers.js
+++ backend/controllers/productControllers.js
@@ -19,7 +19,46 @@ const getProductById = async (req, res) => {
     res.json(product)
 }
 
+const addToCart= async (req, res) => {
+    const { tid } = req.body ;
+    try {
+        const addToCart = await prisma.kosar.create(
+            {
+                data:{
+                    floID:req.user.id,
+                    TmkID:tid
+                }
+            }
+        )
+        res.json("Kosárba helyezve!")
+    } catch (err) {
+        res.json(err.message)
+    }
+}
+
+const kosarTartalom= async (req, res) => {
+    try {
+        const kosar= await prisma.kosar.findMany(
+            {
+                where:{
+                    floID:req.user.id
+                },
+                select:{
+                    id:false,
+                    floID:true,
+                    TmkID:true,
+                }
+            }
+        )
+        res.json(kosar)   
+    } catch (err) {
+        res.json(err.message)
+    }
+}
+
 module.exports = {
     getAllProduct,
-    getProductById
+    getProductById,
+    addToCart,
+    kosarTartalom
 }
--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,9 +1,12 @@
 const express=require('express');
 const router = express.Router();
 
-const {getAllProduct, getProductById} = require('../controllers/productControllers')
+const {protect}=require("../middlewear/authMiddlewear");
+const {getAllProduct, getProductById , addToCart , kosarTartalom} = require('../controllers/productControllers')
 
 router.get('/all',getAllProduct);
 router.post('/getProduct', getProductById)
+router.post("/addCart" , protect , addToCart)
+router.post("/kosaram", protect , kosarTartalom)
 
 module.exports= router;
